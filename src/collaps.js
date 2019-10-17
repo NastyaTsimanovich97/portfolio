9d536ae2a25e984b0e63d8d100b5281e9f7a561e
Nastya Tsimanovich <nastya.cimanovich.97@gmail.com>
2019-10-17 18:27:35 +0300

    feat: add projects

--- src/collaps.js
+++ src/collaps.js
@@ -11,3 +11,16 @@ for (let i = 0; i < coll.length; i++) {
     } 
   });
 }
+
+const mobileCollapse=document.getElementsByClassName("description__mobile");
+for (let i = 0; i < mobileCollapse.length; i++) {
+  mobileCollapse[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    let content = this.nextElementSibling;
+    if (content.style.maxHeight){
+        content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    } 
+  });
+}
