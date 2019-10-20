720eaeba0c9a94de41d6dc717d656bbda3eac810
Nastya Tsimanovich <nastya.cimanovich.97@gmail.com>
2019-10-20 20:50:33 +0300

    feat: add button theyalow

--- src/button.js
+++ src/button.js
@@ -1,5 +1,35 @@
 let el=document.querySelector('iframe');
 
+document.querySelector('.theyallow-button__mobile').addEventListener('click',function(){
+    let buttonMobile= document.querySelector('.theyallow-button__mobile')
+    let buttonDesktop= document.querySelector('.theyallow-button__desktop')
+    let container= document.querySelector('.iframe-container')
+    let main= document.querySelector('.main')
+ 
+
+    el.style.width='640px';
+    
+    main.style=`display:flex;
+                flex-direction:row;`;
+    container.style=`display: inline;
+                        width: 29vw`;
+    buttonMobile.style.top='77%';
+    buttonDesktop.style.top='70%';
+    console.log(buttonMobile)
+    console.log(el)
+ });
+ document.querySelector('.theyallow-button__desktop').addEventListener('click',function(){
+    let container= document.querySelector('.iframe-container')
+    let buttonMobile= document.querySelector('.theyallow-button__mobile')
+    let buttonDesktop= document.querySelector('.theyallow-button__desktop')
+    el.style.width='1440px'
+
+    container.style=`display:none`;
+    buttonMobile.style.top='70%';
+    buttonDesktop.style.top='77%';
+     console.log(el)
+ });
+
 document.querySelector('.button__mobile').addEventListener('click',function(){
    let buttonMobile= document.querySelector('.button__mobile')
    let buttonDesktop= document.querySelector('.button__desktop')
